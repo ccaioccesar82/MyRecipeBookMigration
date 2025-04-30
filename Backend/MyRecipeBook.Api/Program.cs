4537c2d463e6583bc8fb41cbc410e54691ad494b
ccaioccesar82 <ccaioccesar23@gmail.com>
2025-04-30 13:39:59 -0300

    Configurando a api para exigir token nas chamadas de requisição

--- Backend/MyRecipeBook.Api/Program.cs
+++ Backend/MyRecipeBook.Api/Program.cs
@@ -1,6 +1,8 @@
 using MyRecipeBook.Api.Converters;
 using MyRecipeBook.Api.Middlewares;
 using MyRecipeBook.Infrastructure.Migrations;
+using Microsoft.OpenApi.Models;
+using System.Security.Cryptography.Xml;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +11,38 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new StringConverter()));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header usando o Bearer scheme.
+                        Escreva 'Bearer'[espaço] e seu token na caixa de texto abaixo.
+                         Exemplo: Bearer 12345sdsdsds",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+        new OpenApiSecurityScheme{
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer"
+        },
+
+        Scheme = "oauth2",
+        Name = "Bearer",
+        In = ParameterLocation.Header
+     },
+        new List<string>()
+        }
+    });
+});
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
